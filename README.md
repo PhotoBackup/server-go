d24ca880f172e52a4d1a159e06c3c095ec36b731
Nick Thomas <me@ur.gs>
2015-09-21 01:47:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Golang PhotoBackup server
+
+Browsing through F-Droid one day, I came across
+[PhotoBackup](https://photobackup.github.io/) - ever since I stopped using
+[Owncloud[(https://www.owncloud.org), the lack of automatic photo uploads from
+my phone to my server has been bugging me, so I installed the client and started
+looking at the server. The concept is reasonable, the API is simple, but the
+implementations are a bit terrifying, so here's my attempt.
+
+It's written in Go to avoid runtime dependencies; some glaring errors in the
+Python implementation have been corrected. To build from source (assuming
+you already have a Go runtime):
+
+		$ go get github.com/lupine/go-photobackup-server
+		$ cd $GOPATH/src/github.com/lupine/go-photobackup-server
+		$ go build
+		$ cp config.example ~/.photobackup
+		$ vi ~/.photobackup # Add a Password= or PasswordBcrypt= line
+		$ ./photobackup-server-go
+
+There's now a HTTP server running on 127.0.0.1:8420 that will upload to 
+./incoming with the given password. 
+
+If there's demand, I'll put up some precompiled blobs. I wouldn't generally
+recommend *using* precompiled blobs you find on the Internet, though.
+
+Generating bcrypt hashes to stick into PasswordBcrypt is a bit of a pain, so
+here's a oneliner to do it in Ruby:
+
+	
+## Deployment strategy
+
+I'd say stick it under runit or systemd, bound to
+localhost, behind a HTTPS reverse proxy (nginx, say). For bonus points, run it
+as an unprivileged user in a jail. This is made much easier by the lack of
+runtime dependencies, of course.
+
+Here's an nginx reverse proxy directive:
+
+		location /photobackup {
+		    proxy_pass http://127.0.0.1:8420;
+		}
+
+Here's a systemd unit file:
+
+		[Unit]
+		Description=HTTP server for PhotoBackup
+		After=network.target
+
+		[Service]
+		ExecStart=/home/lupine/bin/photobackup-server-go
+		User=lupine
+		WorkingDirectory=/home/lupine
+		Restart=always
+
+
+## Features
+
+### Improvements on photobackup-python
+* Stores the secret on disc in a different format to on the wire (bcrypt(sha512(secret))
+* Constant-time comparison of the secret, to avoid timing attacks
+* Doesn't have to bind to the IPv4 wildcard
+* Supports HTTP prefixes
+* Uses 405 Method Not Allowed where appropriate
+* Uses 409 Conflict where an upload would overwrite an existing file
+* Doesn't read POST data beyond the filesize parameter
+
+
+### To add
+* Direct HTTPS support
+* Sensible multi-user support (support for, say, POST /:username[/test])
+* Config file management like the python version's "init" stuff.
+
---
+++ main.go
@@ -0,0 +1,331 @@
+//
+// This is a small HTTP server implementing the "photobackup server" endpoint
+// documented here: https://github.com/PhotoBackup/api/
+//
+// Written because the existing servers make me a touch sad; go means we can
+// avoid a pile of runtime dependencies. Build-time dependencies are being kept
+// low; bcrypt, homedir and graceful are the only luxuries. Adding gorilla mux
+// and, perhaps, negroni, would probably be overkill.
+//
+// We're trying to be compatible, so config file is INI-format: ~/.photobackup
+// 	   [photobackup]
+// 	   MediaRoot=<path to store files in>
+//	   Password=<sha512 digest of password, no salt>
+//	   Port=<port>
+//
+// In addition to these keys, I'm also supporting:
+// 	   BindAddress=<address to bind to>
+// 	   PasswordBcrypt=<bcrypt of sha512 digest of plain password>
+// 	   HTTPPrefix=<prefix to mount HTTP server at>
+//
+// The original server was intended to run over HTTP, I think, hence the client
+// sending a SHA512'd password. We support this scheme, but the on-disc storage
+// format is really better off being bcrypt(sha512(password)), so I've added
+// that.
+//
+// Adding BindAddress and HTTPPrefix means that mounting this behind a HTTP
+// reverse proxy is quite doable, and lets us offload HTTPS to that as well.
+// That's how I'm intending to use it.
+//
+// I think the original servers are designed so you can connect to them using
+// just HTTP; hence the sha512(password) scheme. This is short-sighted; the only
+// thing it gets you is (weak) protection against sniffing if you happen to use
+// the same password elsewhere. Sniffers in this scenario can still upload to
+// your server and view your photos.
+//
+// At some point in the future I might add direct HTTPS support as well, but I
+// don't need it.
+//
+// @author Nick Thomas <photobackup@ur.gs>
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/Unknwon/goconfig"
+	"github.com/mitchellh/go-homedir"
+	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/tylerb/graceful.v1"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	MediaRoot      string // mandatory
+	Port           string // optional, defaults to 8420
+	BindAddress    string // Optional, defaults to 127.0.0.1
+	PasswordBcrypt string // Either this or Password
+	HTTPPrefix     string // Optional, defaults to "/"
+}
+
+// Implements the API defined here: https://github.com/PhotoBackup/api
+type Handler struct {
+	Config *Config
+}
+
+func (h *Handler) BuildMux() http.Handler {
+	mux := http.NewServeMux()
+
+	prefix := h.Config.HTTPPrefix
+	if prefix == "/" {
+		prefix = ""
+	}
+
+	// We don't use http.StripPrefix because that leaves a path of "" for "/foo"
+	if len(prefix) > 0 {
+		mux.HandleFunc(prefix, h.RootHandler)
+	}
+
+	mux.HandleFunc(prefix+"/", h.RootHandler)
+
+	mux.HandleFunc(prefix+"/test", h.TestHandler)
+	mux.HandleFunc(prefix+"/test/", h.TestHandler)
+
+	return mux
+}
+
+// TODO: this lot could be refactored into smaller methods
+func (h *Handler) RootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		http.Redirect(w, r, "https://photobackup.github.io/", 302)
+		return
+	}
+
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+
+	if !h.CheckPassword(w, r) {
+		return
+	}
+
+	filesize, ok := h.ReadFileSize(w, r)
+	if !ok {
+		return
+	}
+
+	upfile, upfileHdr, err := r.FormFile("upfile")
+	if upfile == nil || upfileHdr == nil || upfileHdr.Filename == "" || err != nil {
+		if err == nil {
+			err = errors.New("Unknown error")
+		}
+
+		// No idea why the spec demands 401 here
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Failed to read upfile parameter: " + err.Error()))
+
+		return
+	}
+
+	filename := filepath.Join(h.Config.MediaRoot, upfileHdr.Filename)
+
+	// This will always be vulnerable to races, but here we check if the
+	// file already exists, and return a 409 if it does. This isn't in the
+	// RAML, and the "official" Python API returns nothing AFAICT. This is
+	// definitely an improvement over that!
+	if _, err := os.Stat(filename); !os.IsNotExist(err) {
+		w.WriteHeader(http.StatusConflict)
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error opening file: " + err.Error()))
+		return
+	}
+
+	// Try to read `filesize` bytes into the destination. An io.EOF says
+	// that filesize was too low; anything else was our fault.
+	_, err = io.CopyN(file, upfile, filesize)
+
+	if err != nil {
+		if err == io.EOF {
+			w.WriteHeader(http.StatusLengthRequired)
+			w.Write([]byte("filesize larger than upfile data"))
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Error writing file: " + err.Error()))
+		}
+
+		// best-effort `rm' here.
+		_ = os.Remove(filename)
+
+		return
+	}
+
+	// Now we try to read one more byte. If that *doesn't* return EOF, then
+	// filesize was too low.
+	//
+	// What we don't do here is read the entire file before deciding that
+	// it was too big, as the Python server does.
+	tmp := make([]byte, 1)
+	if _, err := upfile.Read(tmp); err == nil {
+		w.WriteHeader(http.StatusLengthRequired)
+		w.Write([]byte("upfile data larger than filesize"))
+
+		// best-effort remove here too
+		_ = os.Remove(filename)
+
+		return
+	}
+
+	if err := file.Close(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error closing written file: " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) TestHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		return
+	}
+
+	if !h.CheckPassword(w, r) {
+		return
+	}
+
+	filename := filepath.Join(h.Config.MediaRoot, ".test_file_to_write")
+	_ = os.Remove(filename)
+	defer os.Remove(filename)
+
+	file, err := os.Create(filename)
+	if err == nil {
+		err = file.Close()
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Error opening file: " + err.Error()))
+		return
+	}
+}
+
+// The multipart/mime form-data "password" the hexdigested sha512'd password
+// We bcrypt() it then check it against the password in the config file in a
+// way that *isn't* vulnerable to timing attacks.
+//
+// Side effect: We write a 403 to w if the password is bad or missing
+func (h *Handler) CheckPassword(w http.ResponseWriter, r *http.Request) bool {
+	// TODO: get rid of these conversions
+	actual := []byte(h.Config.PasswordBcrypt)
+	putative := []byte(r.FormValue("password"))
+	ok := (bcrypt.CompareHashAndPassword(actual, putative) == nil)
+
+	if !ok {
+		w.WriteHeader(http.StatusForbidden)
+	}
+
+	return ok
+}
+
+// Reads the filesize parameter from the request.
+//
+// Side effect: Writes a 400 if the filesize param isn't present or doesn't
+// convert to a positive int64. The spec allows the client to specify a negative
+// number but I'm not interested in that.
+func (h *Handler) ReadFileSize(w http.ResponseWriter, r *http.Request) (int64, bool) {
+	str := r.FormValue("filesize")
+	if str == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("filesize not set"))
+		return -1, false
+	}
+
+	filesize, err := strconv.ParseInt(str, 10, 64)
+	if err != nil || filesize < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("filesize does not parse to a 64-bit int > 0"))
+		return -1, false
+	}
+
+	return filesize, true
+}
+
+func GetConfig() (*Config, error) {
+	var ConfigFile string
+
+	if len(os.Args) == 2 {
+		ConfigFile = os.Args[1]
+	} else {
+		if base, err := homedir.Dir(); err != nil {
+			return nil, err
+		} else {
+			ConfigFile = filepath.Join(base, ".photobackup")
+		}
+	}
+
+	cfg, err := goconfig.LoadConfigFile(ConfigFile)
+	if err != nil {
+		return nil, err
+	}
+	section, err := cfg.GetSection("photobackup")
+	if err != nil {
+		return nil, err
+	}
+
+	out := Config{
+		MediaRoot:      section["MediaRoot"],
+		Port:           section["Port"],
+		BindAddress:    section["BindAddress"],
+		PasswordBcrypt: section["PasswordBcrypt"],
+		HTTPPrefix:     section["HTTPPrefix"],
+	}
+
+	if out.MediaRoot == "" {
+		return nil, errors.New("MediaRoot not specified")
+	}
+	// TODO: check MediaRoot is a valid, writable path?
+
+	// If Password and PasswordBcrypt are both set, the latter wins
+	if pwd, ok := section["Password"]; ok && out.PasswordBcrypt == "" {
+		hash, err := bcrypt.GenerateFromPassword([]byte(pwd), bcrypt.DefaultCost)
+		if err != nil {
+			return nil, errors.New("Failed to convert Password to PasswordBcrypt: " + err.Error())
+		}
+		out.PasswordBcrypt = string(hash)
+	}
+
+	if out.PasswordBcrypt == "" {
+		return nil, errors.New("Neither Password nor PasswordBcrypt specified")
+	}
+
+	if out.BindAddress == "" {
+		out.BindAddress = "127.0.0.1"
+	}
+
+	if out.Port == "" {
+		out.Port = "8420"
+	}
+
+	return &out, nil
+}
+
+func main() {
+
+	if len(os.Args) > 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s [config-file]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	config, err := GetConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't read config: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	listenOn := net.JoinHostPort(config.BindAddress, config.Port)
+	api := &Handler{Config: config}
+	mux := api.BuildMux()
+
+	graceful.Run(listenOn, 10*time.Second, mux)
+}
